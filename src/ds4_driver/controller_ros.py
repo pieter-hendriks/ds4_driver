0f0f85bbb09daa8ab2b8c8ca8abfb1f0c235e650
Pieter Hendriks <pieter.hendriks@student.uantwerpen.be>
2021-08-30 19:06:18 +0200

    Fix

--- src/ds4_driver/controller_ros.py
+++ src/ds4_driver/controller_ros.py
@@ -24,7 +24,7 @@ class ControllerRos(Controller):
 		speed = -0.3
 		turn = -0.34
 		base =0
-		for i in range(2):
+		for i in range(1):
 			checkpoints.append((base+time, speed, turn))
 			checkpoints.append((base+time+time, -1*speed, -1*turn))
 			base += 2*time
